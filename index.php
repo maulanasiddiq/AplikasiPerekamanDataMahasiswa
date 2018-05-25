9bbd05d7197d802c2ae857478be44f5ddfd27262
Maulana Abdul Siddiq <siddiqmaulanaabdul@gmail.com>
2018-05-25 16:32:16 +0700

    commit 19

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
-    <link rel="shortcut icon" href="favicon.ico">
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
     <script type="text/javascript" src="assets/js/jquery.js"></script>
     <script type="text/javascript" src="assets/js/bootstrap.js"></script>
