13ce0431e6d88e2b3f0869acf6a0d0bf88f8e5fe
Maulana Abdul Siddiq <siddiqmaulanaabdul@gmail.com>
2018-05-25 23:16:43 +0700

    commit 24

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div class="container">
 	<br>
 	<hr>
-	<a href="instagram.com/maulanaabdulsiddiq">Copyright &copy; 2018 Created by Maulana Abdul Siddiq</a>
+	Copyright &copy; 2018 Created by Maulana Abdul Siddiq
 	<br>
 	<br>
 </div>
