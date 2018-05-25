357e2909e9724483dbab7cc179a43411d425769c
Maulana Abdul Siddiq <siddiqmaulanaabdul@gmail.com>
2018-05-25 15:44:27 +0700

    commit 13

--- header.php
+++ header.php
@@ -1,3 +1,9 @@
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+<script type="text/javascript" src="assets/js/jquery.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.js"></script>
+
 <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
   <div class="container">
     <a class="navbar-brand" href="index.php">Aplikasi Remas</a>
